130b8d760495594d789cf737da168fdfa0d79947
John Dyer <john@krumpt.com>
2010-04-19 00:33:56 +0000

    added password edit to admin/index.php

--- formUtilityFunctions/updatePass.php
+++ formUtilityFunctions/updatePass.php
@@ -1,10 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>		
+	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
 <?
 include '../functions.inc';
 
-$inputConfirmPassword = $_REQUEST['inputConfirmPassword'];
-
-setPassword(trim(sha1($inputConfirmPassword)));
-
-redirectTo("../adminPage.php");
-
+$oldPassword = $_REQUEST['oldPassword'];
+$newPassword = $_REQUEST['newPassword'];
 ?>
+</head>
+<?
+$result = setPassword(trim($oldPassword),trim(sha1($newPassword)));
+//redirectTo("../adminPage.php");
+switch ($result) {
+	case '1':
+		echo "changed";
+		break;
+	case '0':
+		echo "not changed";
+		break;
+	case 'error':
+		echo "error";
+		break;
+	default:
+		echo "default";
+		break;
+	}
+?>	
+<script type="text/javascript" charset="utf-8">
+	parent.Mediabox.closerefresh();
+</script>
+</html>
